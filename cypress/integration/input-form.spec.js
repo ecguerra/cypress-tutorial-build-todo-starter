f64b282c9b34db55ede1bfbe9ceaf433a8be8620
ecguerra <elizabethcguerra@gmail.com>
2022-01-12 11:19:01 -0500

    text inputs

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,18 @@
 describe('input form', ()=>{
+    beforeEach(()=>{
+        cy.visit('/')
+    })
+
     it('focuses input on load', ()=>{
-        cy.visit('http://localhost:3030')
+        cy.focused() // had to add autoFocus to TodoForm
+          .should('have.class', 'new-todo')
+    })
+
+    it('accepts input', ()=>{ // .only = this test runs by itself
+        const typedText ='Buy Milk'
+
+        cy.get('.new-todo') // better to use get instead of focus - keep tests isolated from unrelated features
+          .type(typedText)
+          .should('have.value', typedText)
     })
 })
