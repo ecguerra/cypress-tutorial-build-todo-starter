3adaf1708c9ccc6a5e29346be275b17dee6d812f
ecguerra <elizabethcguerra@gmail.com>
2022-01-13 16:38:21 -0500

    E2E testing pt 2 & headless testing implemented

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -7,6 +7,9 @@
 // For simplicity/lightweight - focus on happy path types and each network request
 // 1 test each for GET, POST, PUT, DELETE
 
+// to run all tests headless, add "cypress:all": "cypress run" to scripts in package.json
+// then npm run cypress:all to see outputs in terminal
+
 describe('Smoke tests', ()=>{
   beforeEach(()=>{
     cy.request('GET', '/api/todos')
@@ -15,7 +18,7 @@ describe('Smoke tests', ()=>{
   }) // starts us with an empty DB // not the most efficient method, look into other options
 
   context('With no todos', ()=>{
-    it.only('Saves new todos', ()=>{
+    it('Saves new todos', ()=>{
       const items = [
         {text: 'Buy milk', expectedLength: 1},
         {text: 'Buy eggs', expectedLength: 2},
@@ -37,7 +40,65 @@ describe('Smoke tests', ()=>{
           cy.get('.todo-list li')
             .should('have.length', todo.expectedLength)
         })
+    })
+  })
+  
+  context('With active todos', ()=>{
+    beforeEach(()=>{
+      cy.fixture('todos')
+        .each(todo => {
+          const newTodo = Cypress._.merge(todo, {isComplete: false})
+          cy.request('POST', '/api/todos', newTodo)
+        })
+        cy.visit('/')
+    })
 
+    it('loads existing data from DB', ()=>{
+      cy.get('.todo-list li')
+        .should('have.length', 4)
+    })
+
+    it('deletes todos', ()=>{
+      cy.server()
+      cy.route('DELETE', '/api/todos/*')
+        .as('delete')
+
+      cy.get('.todo-list li')
+        .each($el=>{
+          cy.wrap($el)
+            .find('.destroy')
+            .invoke('show')
+            .click()
+
+          cy.wait('@delete')
+        })
+        .should('not.exist')
+    })
+
+    it('toggles todos', ()=>{
+      const clickAndWait = ($el) =>{
+        cy.wrap($el)
+          .as('item')
+          .find('.toggle')
+          .click()
+
+        cy.wait('@update')
+      }
+        cy.server()
+        cy.route('PUT', '/api/todos/*')
+          .as('update')
+        
+        cy.get('.todo-list li')
+          .each($el=>{
+            clickAndWait($el)
+            cy.get('@item')
+              .should('have.class', 'complete')
+        })
+          .each($el=>{
+            clickAndWait($el)
+            cy.get('@item')
+              .should('not.have.class', 'complete')
+        })
     })
   })
 })
